b17beda251a56f32f48c6d0d83a6394b187c9bf4
Vincent Franstyo <sorakiriko28814@gmail.com>
2023-12-09 14:33:24 +0700

    store

---
+++ types/models.tsx
@@ -0,0 +1,10 @@
+interface other_services{
+    id: number,
+    username: string,
+    topic: string,
+    description: string,
+    preferredSolution: string
+    resolved: boolean
+}
+
+export type {other_services}

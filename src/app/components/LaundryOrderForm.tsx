1641865f293ffb71a7e3313659118e7ae1dc5ec3
Inas <102895578+adtyainash@users.noreply.github.com>
2023-12-09 12:26:46 +0700

    Update LaundryOrderForm.tsx
    fix: fix bug

--- src/app/components/LaundryOrderForm.tsx
+++ src/app/components/LaundryOrderForm.tsx
@@ -1,46 +1,45 @@
 'use client'
-
 import React, { useState, ChangeEvent, FormEvent } from 'react';
 import { useSession } from 'next-auth/react';
 
 export default function Laundry() {
-    const { data: session } = useSession();
-    const [weight, setWeight] = useState('');
-    const [totalPrice, setTotalPrice] = useState(0);
+  const { data: session } = useSession();
+  const [weight, setWeight] = useState('');
+  const [totalPrice, setTotalPrice] = useState(0);
 
   const handleWeightChange = (e: ChangeEvent<HTMLInputElement>) => {
     const newWeight = parseInt(e.target.value, 10);
-    
+
     if (!isNaN(newWeight) && newWeight >= 0) {
       setWeight(newWeight.toString());
-  
+
       // Calculate total price based on the formula (7000 * weight)
       const newTotalPrice = 7000 * newWeight;
       setTotalPrice(newTotalPrice);
     }
   };
-  
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
 
-        // Call your API endpoint to create a laundry order
-        const res = await fetch('/api/layanan/laundry', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                residentId: session?.user?.id, // Assuming you have the residentId in the session
-                weight: parseInt(weight, 10),
-            }),
-        });
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        // Handle the response from the server as needed
-        const result = await res.json();
-        console.log(result);
-    };
+    // Call your API endpoint to create a laundry order
+    const res = await fetch('/api/layanan/laundry', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        residentId: session?.user?.id, // Assuming you have the residentId in the session
+        weight: parseInt(weight, 10),
+      }),
+    });
 
-  return {
+    // Handle the response from the server as needed
+    const result = await res.json();
+    console.log(result);
+  };
+
+  return (
     <div className="max-w-md mx-auto p-4 border rounded shadow-md">
       <form onSubmit={handleSubmit} className="flex flex-col items-center">
         <label className="mb-4">

fa201577f4cce5d8e6ba2476172bd5bbf09bc1f5
Vincent Franstyo <sorakiriko28814@gmail.com>
2023-12-09 14:35:42 +0700

    haha

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,17 +1,17 @@
 "use client"
 import Link from "next/link";
 import Image from "next/image";
-import { useSession } from "next-auth/react"
+import {useSession} from "next-auth/react"
 import NavButtonSignout from "@/app/components/NavButtonSignout"
-import { FaHome } from "react-icons/fa";
+import {FaHome} from "react-icons/fa";
 
 const Navbar = () => {
     const {data: session} = useSession();
 
     return (
-        <nav className="flex flex-between w-full pt-4 justify-between px-20 mt-10">
+        <nav className="flex flex-between w-full pt-4 justify-between px-20 mt-10 mb-16">
             <Link href="/" className="flex gap-4 flex-center">
-                <FaHome size={30} />
+                <FaHome size={30}/>
                 <p className="mt-2">UniLodge</p>
             </Link>
 
@@ -25,11 +25,15 @@ const Navbar = () => {
                         {/*    className="search_input peer"*/}
                         {/*    // todo implement functioning search bar*/}
                         {/*/>*/}
-                        <Link href="/" className="hover:underline mt-2">File complaint</Link>
-                        <Link href="/" className="hover:underline mt-2">Food</Link>
-                        <Link href="/pages/laundry" className="hover:underline mt-2">Laundry</Link>
-                        <Link href="/" className="hover:underline mt-2">Others</Link>
-                        <Link href="/" className="hover:underline mt-2">Chat App</Link>
+                        {session?.user.role === "resident" && (
+                            <>
+                                <Link href="/pages/complaint" className="hover:underline mt-2">File complaint</Link>
+                                <Link href="/pages/food" className="hover:underline mt-2">Food</Link>
+                                <Link href="/pages/laundry" className="hover:underline mt-2">Laundry</Link>
+                                <Link href="/pages/otherservices" className="hover:underline mt-2">Others</Link>
+                                <Link href="/pages/chat" className="hover:underline mt-2">Chat App</Link>
+                            </>
+                        )}
                         {session?.user.role === "admin" && (
                             <Link href="/" className="hover:underline mt-2">Complaints</Link>
                         )}
@@ -48,10 +52,12 @@ const Navbar = () => {
                 ) : (	// else
                     // todo ganti ke sign in sama register option beneran
                     <div className="flex flex-between gap-4 md:gap-4">
-                        <Link href="/pages/register" className="mx-4 bg-white rounded-full bg-transparent py-2.5 px-5 text-black transition-all hover:bg-black hover:text-white text-center text-sm font-inter flex items-center justify-center">
+                        <Link href="/pages/register"
+                              className="mx-4 bg-white rounded-full bg-transparent py-2.5 px-5 text-black transition-all hover:bg-black hover:text-white text-center text-sm font-inter flex items-center justify-center">
                             Register
                         </Link>
-                        <Link href="/pages/login" className="mr-4 rounded-full bg-blue-500 py-2.5 px-5 text-white transition-all hover:bg-white hover:text-black text-center text-sm font-inter flex items-center justify-center">
+                        <Link href="/pages/login"
+                              className="mr-4 rounded-full bg-blue-500 py-2.5 px-5 text-white transition-all hover:bg-white hover:text-black text-center text-sm font-inter flex items-center justify-center">
                             Sign In
                         </Link>
                     </div>
